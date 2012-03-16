a09ab1d4f222cef94ae085304d3c9e03098eef6f
Peter Fern <github@obfusc8.org>
2012-03-16 23:27:57 +1100

    Don't send auth for network_meta requests (fixes pull)

--- lib/commands/helpers.rb
+++ lib/commands/helpers.rb
@@ -388,7 +388,7 @@ helper :github_token do
 end
 
 helper :cache_data do |user|
-  `curl -s -L -F 'login=#{github_user}' -F 'token=#{github_token}' #{network_meta_for(user)} -o #{network_cache_path}`
+  `curl -s -L #{network_meta_for(user)} -o #{network_cache_path}`
   get_cache
 end
 
